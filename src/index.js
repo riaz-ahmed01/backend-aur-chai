f2d6e981dffbb37c1f4820a0ca40d2f93f35be37
riaz ahmed <riazqazi002@gmail.com>
2025-02-27 12:31:10 +0500

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -7,33 +7,10 @@ dotenv.config();
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 3000, () => {
-      console.log(`Server is running at http://locaalhost:${process.env.PORT}`);
+      console.log(`Server is running at http://localhost:${process.env.PORT}`);
     });
   })
   .catch(() => {
     console.log("MongoDB Connection FAILED");
   });
 
-/*
-import express from "express";
-
-const app = express();
-
-
-(async () => {
-  try {
-    awaitmongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-    app.on("error", (error) =>{
-        console.log(error);
-        throw error
-        
-    })
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is running at http://loacalhost:${process.env.PORT}`);
-        
-    })
-  } catch (error) {
-    console.log(error);
-  }
-})();
-*/
---
+++ src/utils/ApiError.js
@@ -0,0 +1,17 @@
+class ApiError extends Error {
+  constructor(statusCode, message, errors = [], data = null, stack = "") {
+    super(message);
+    this.statusCode = statusCode;
+    this.errors = errors;
+    this.success = false;
+    this.data = null; 
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export default ApiError;
