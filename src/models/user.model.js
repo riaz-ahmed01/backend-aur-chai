9d1b733d13142e7ee16444d07ae6734d4bf59063
riaz ahmed <riazqazi002@gmail.com>
2025-02-28 18:42:38 +0500

    add bcrypt jwt aggregatePaginate

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,56 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avtar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // cloudinary url
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-},{timestamps:true})
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-export const User = mongoose.model("User", userSchema)
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign({_id :this._id})
+}
+
+userSchema.methods.generateRefreshToken = function(){}
+
+export const User = mongoose.model("User", userSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,38 +1,45 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new mongoose.Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-      index: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    fullname: {
-      type: String,
-      required: true,
-      trim: true,
-      index: true,
-    },
-    avtar:{
+     videoFile:{
+        type:String, //coludinary url
+        required:true,
+     },
+     thumbnail:{
         type:String, // cloudinary url
         required:true,
-    },
-    coverImage:{
-        type:String, // cloudinary url
+     },
+     title:{
+        type:String, 
         required:true,
-    }
+     },
+     description:{
+        type:String, 
+        required:true,
+     },
+     duration:{
+        type:Number,
+        required:true,
+     },
+     views:{
+      type:Number,
+      default:0
+     },
+     isPublished:{
+       type:Boolean,
+       default:true,
+     },
+     owner:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"User"
+     },
+
   },
   { timestamps: true }
 );
+
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model("Video", videoSchema);
